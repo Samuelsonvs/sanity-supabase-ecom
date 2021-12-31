a7f11d02148126524e21908bebff5556e5efc293
Samuelsonvs <m.sametatali@gmail.com>
2021-12-31 23:45:26 +0300

    Add auth

--- pages/profile/index.tsx
+++ pages/profile/index.tsx
@@ -1,8 +1,10 @@
 import type { NextPage } from "next";
 import React from "react";
 
+import withAuth from "@/container/AuthContainer";
+
 export const Profile: NextPage = () => {
   return <div>Profile Page</div>;
 };
 
-export default Profile;
+export default withAuth(Profile);
